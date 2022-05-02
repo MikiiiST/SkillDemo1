d57a076c278195a786991761d9cc19991837247b
MikiiiST <wyszeto@ucsd.edu>
2022-05-01 23:31:15 -0700

    skillDemo1

--- skillDemo1Week5.java
+++ skillDemo1Week5.java
@@ -1,3 +1,7 @@
 public class skillDemo1Week5 {
 
+    public static int area(int x, int y) {
+        return x*y;
+    }
+
 }
---
+++ skillDemo1Week5Test.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class skillDemo1Week5Test {
+    
+    @Test
+    public void areaTest() {
+        assertEquals(8, skillDemo1Week5.area(2,3)); //test that fails
+    }
+
+}
